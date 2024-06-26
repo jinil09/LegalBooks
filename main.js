12c538feb24f97e36370026658585fb5fd92ba53
Jinil Prajapati <109332175+jinil09@users.noreply.github.com>
2024-06-26 14:31:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-document.getElementById('consultation-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('OTP Sent!');
+function togglePopup() {
+  const popup = document.getElementById('customPopup');
+  popup.classList.toggle('hidden-custom-popup');
+}
+
+function closePopup() {
+  const popup = document.getElementById('customPopup');
+  popup.classList.add('hidden-custom-popup');
+}
+
+function toggleSpecializations(checkbox, specializationId) {
+  const specializations = document.getElementById(specializationId);
+  const checkboxes = specializations.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach(cb => cb.checked = checkbox.checked);
+}
+
+document.addEventListener('click', function(event) {
+  const popup = document.getElementById('customPopup');
+  const button = document.querySelector('.input-field.mb-2.md\\:mb-0.bg-blue-500');
+  if (!popup.contains(event.target) && !button.contains(event.target) && !popup.classList.contains('hidden-custom-popup')) {
+      closePopup();
+  }
 });
